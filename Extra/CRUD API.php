2290b51cda7a956e33b2394d086e9c20f80e547f
Mohamed-Ebeid <mohmedalhkim@gmail.com>
2023-07-29 21:17:32 +0300

    Added some extra files

---
+++ Extra/CRUD API.php
@@ -0,0 +1,164 @@
+CMD: php artisan make:migration create_blogs_table
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255)->nullable();
+            $table->text('body')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogs');
+    }
+}
+
+####################################################################################################################################################
+CMD: php artisan migrate
+
+--routes/api.php
+
+use App\Http\Controllers\BlogController;
+Route::resource('blogs', BlogController::class);
+
+CMD: php artisan make:controller BlogController --resource –model=Blog
+
+--app/Http/Controllers/BlogController.php
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Blog;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $blogs = Blog::latest()->paginate(10);
+        return [
+            "status" => 1,
+            "data" => $blogs
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $blog = Blog::create($request->all());
+        return [
+            "status" => 1,
+            "data" => $blog
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Blog $blog)
+    {
+        return [
+            "status" => 1,
+            "data" =>$blog
+        ];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        $blog->update($request->all());
+
+        return [
+            "status" => 1,
+            "data" => $blog,
+            "msg" => "Blog updated successfully"
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Blog  $blog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return [
+            "status" => 1,
+            "data" => $blog,
+            "msg" => "Blog deleted successfully"
+        ];
+    }
+}
+####################################################################################################################################################
