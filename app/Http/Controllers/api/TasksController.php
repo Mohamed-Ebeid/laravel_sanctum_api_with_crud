62103b34d874149c5dec81b265ea3147b930ee80
Mohamed-Ebeid <mohmedalhkim@gmail.com>
2023-07-29 21:03:44 +0300

    Done with show a single task, edit a tast and delete a task

--- app/Http/Controllers/api/TasksController.php
+++ app/Http/Controllers/api/TasksController.php
@@ -3,14 +3,17 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\TaskResource;
+use App\Http\Resources\TaskResource; //resource is for stucturing and JSON
+use App\Http\Requests\StoreTaskRequest; //Request is for validation
 use App\Models\Task;
+use App\Traits\HttpResponses;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class TasksController extends Controller
 {
+    use HttpResponses;
     /**
      * Display a listing of the resource.
      */
@@ -28,52 +31,68 @@ class TasksController extends Controller
          
          
     }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+  
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreTaskRequest $request)
     {
-        //
+        $request->validated($request->all());
+
+        $task = Task::create([
+            'user_id'=> Auth::user()->id,
+            'name'=>$request->name,
+            'description'=>$request->description,
+            'priority'=>$request->priority,
+        ]);
+
+        return new TaskResource($task);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Task $task)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
+        // $task = Task::where('id', $id)->get();
+        return new TaskResource($task);
+        //return $task;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(StoreTaskRequest $request, Task $task)
     {
-        //
+        $request->validated($request->all());
+
+        if(Auth::user()->id !== $task->user_id){
+            return $this->error('', 'You are not the owner of this task!', 403);
+        }
+        $task->update($request->all());
+
+        return new TaskResource($task);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Task $task)
     {
-        //
+        if(Auth::user()->id !== $task->user_id){
+            return $this->error('', 'You are not the owner of this task!', 403);
+        }
+
+        $task->delete();
+        return $this->success('', 'Deleted successfully!!', 200);
+        
+        //return $this->notTheOwner($task) ? $this->notTheOwner($task) : $task->delete();
     }
+
+    // private function notTheOwner($task){
+    //     if(Auth::user()->id !== $task->user_id){
+    //         return $this->error('', 'You are not the owner of this task!', 403);
+    //     }
+    // }
 }
